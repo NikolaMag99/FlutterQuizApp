b292485d1fc0caca6b35806da7174fb10486cb41
Nikola Paunovic <nikolapaunovic99@gmail.com>
2021-08-07 18:08:23 +0200

    Dodavanje pitanja i odgovora

--- lib/main.dart
+++ lib/main.dart
@@ -7,12 +7,36 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      'Best tennis player?',
+      'When Novak Djokovic wins first gnad slam?'
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('dr Paun'),
         ),
-        body: Text('Nesto'),
+        body: Column(
+          children: <Widget>[
+            Text('The question!'),
+            ElevatedButton(
+              onPressed: null,
+              child: Text('Nadal'),
+            ),
+            ElevatedButton(
+              onPressed: null,
+              child: Text('Federer'),
+            ),
+            ElevatedButton(
+              onPressed: null,
+              child: Text('Djokovic'),
+            ),
+            ElevatedButton(
+              onPressed: null,
+              child: Text('Zverev'),
+            )
+          ],
+        ),
       ),
     );
   }
